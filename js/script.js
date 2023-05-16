2822694bdac9257028468262d62c9682060be094
Ludvig Alvir <ludvig.alvir@gmail.com>
2023-05-16 14:34:38 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@
 const comment = JSON.stringify({
 	post: 29,
 	/*
-    *** Can add more fields than post and content, but must have those two ***
-    author_name: "YourName",
-	author_email: "YourName@Email.com",*/
+	 *** Can add more fields than post and content, but must have those two ***
+	 */
 	content: "Test comment",
 });
 
@@ -24,19 +23,4 @@ async function postComment() {
 	console.log(res);
 	const data = await res.json();
 	console.log(data);
-}
-
-//Example for fetching all comments for post with id 29
-async function getComments() {
-	const res = await fetch(
-		"https://*YourWordPressSite*/wp-json/wp/v2/comments?post=29"
-	);
-	const data = await res.json();
-	data.forEach((comment) => {
-		document.querySelector("body").innerHTML += `
-        <h2>${comment.author_name}</h2>
-        ${comment.content.rendered}
-        <hr>
-    `;
-	});
 }
